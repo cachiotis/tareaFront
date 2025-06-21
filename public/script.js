668de6c4d3727f36bd8dafc5319282f8537a54d1
Nicashot <nicachot@gmail.com>
2025-06-21 11:58:54 -0500

    first commit

---
+++ public/script.js
@@ -0,0 +1,37 @@
+document.getElementById('tarea').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+const nombre = document.getElementById('nombre').value;
+const descripcion = document.getElementById('descripcion').value;
+
+const tarea = { nombre, descripcion };
+
+try {
+    const response = await fetch('http://localhost:3000/tareas', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(tarea),
+    });
+    const data = await response.json();
+    alert('Usuario creado: ' + JSON.stringify(data));
+    cargarTareas();
+    } catch (err) {
+        console.error('Error creando usuario:', err);
+    }
+});
+
+async function cargarTareas() {
+    try {
+        const response = await fetch('http://localhost:3000/tareas');
+        const tareas = await response.json();
+        const tareasList = document.getElementById('tareaList');
+        tareasList.innerHTML = tareas.map(tarea => `
+            <li>${tarea.nombre}<br>${tarea.descripcion}</li>
+            <br>
+        `).join('');
+    } catch (err) {
+        console.error('Error cargando usuarios:', err);
+    }
+}
+
+cargarTareas();
